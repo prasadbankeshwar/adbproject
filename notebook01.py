696eb53ebad654c20f45236f00575834649cf969
prasadbankeshwar <prasadbankeshwar>
2025-10-29 10:07:54 +0000

    modified notebook01

--- notebook01.py
+++ notebook01.py
@@ -1,7 +1,7 @@
 # Databricks notebook source
 print ("testing")
-print ("testing again on 13Oct25")
+print ("testing again on 29Oct25")
 
 # COMMAND ----------
 
-print("updated notebook on 21Feb25")
+print("updated notebook on 29Oct25")
